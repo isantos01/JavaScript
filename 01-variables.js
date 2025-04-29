56defc25c0aa495a68df5ff1253400a2e6aae825
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-29 15:54:20 -0400

    commit

---
+++ 01-variables.js
@@ -0,0 +1,11 @@
+// var
+var helloWorld = "Hello World!"; // This variable is global
+console.log(helloWorld);
+
+// let 
+let helloWorld2 = "Hello World!"; // This variable can only be access within the same block
+console.log(helloWorld2);
+
+// const
+const helloWorld3 = "Hello World!"; // This variable can only access within the same block and cannot be re-assigned
+console.log(helloWorld3);
