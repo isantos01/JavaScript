e8b3132666b6bda5fd7dc7127c0cebe244ebcd2c
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-30 00:03:13 -0400

    commit

---
+++ 03-beginner-exercises.js
@@ -0,0 +1,90 @@
+// 1. Escribe un comentario en una línea
+
+// Un comentario en una linea
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+Un
+Comentario
+En varias
+lineas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let edad = 18
+let country = "Ireland"
+let isMalicious = false
+let waitingValue
+let emptyNull = null
+let billionaire = BigInt(1222223333333333332222222222222222222222211111111111)
+let symbol = Symbol("red")
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(edad)
+console.log(country)
+console.log(isMalicious)
+console.log(waitingValue)
+console.log(emptyNull)
+console.log(billionaire)
+console.log(symbol)
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof (edad))
+console.log(typeof (country))
+console.log(typeof (isMalicious))
+console.log(typeof (waitingValue))
+console.log(typeof (emptyNull))
+console.log(typeof (billionaire))
+console.log(typeof (symbol))
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+edad = 19
+country = "England"
+isMalicious = true
+waitingValue
+emptyNull = null
+billionaire = BigInt(1222223333333333332222222222222222999999999999991111)
+symbol = Symbol("blue")
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+edad = "19"
+country = 100
+isMalicious = BigInt(444444444444444444444444444444444444444444444444444444)
+waitingValue = null
+emptyNull
+billionaire = Symbol("yellow")
+symbol = false
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const id = 1
+const city = "Austin"
+const verdict = true
+const millionaire = 88888888888888888888888888888888888888888888888888n
+const noValue = null
+const uniqueProperty = Symbol("Gigantic")
+
+
+// 9. A continuación, modifica los valores de las constantes
+
+// id = "Austin"
+// city = 1
+// verdict = 88888888888888888888888888888888888888888888888888n
+// millionaire = true
+// noValue = Symbol("Gigantic")
+// uniqueProperty = null
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+// id = "Austin"
+// city = 1
+// verdict = 88888888888888888888888888888888888888888888888888n
+// millionaire = true
+// noValue = Symbol("Gigantic")
+// uniqueProperty = null
