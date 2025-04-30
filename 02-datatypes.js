61266a3b4aee142ead8557d73f7f65aa607612bd
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-29 23:38:25 -0400

    commit

---
+++ 02-datatypes.js
@@ -0,0 +1,11 @@
+// There seven primary data types
+
+/*
+String
+Numbers
+Boolean
+Undefined
+Null
+Symbol ----> Symbol("symbol_name")
+BigInt -----> BigInt(12333333333334322222222222123) or 12333333333334322222222222123n
+*/
