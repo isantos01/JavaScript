b6067f29a201adbe0ea8dc47da170bae71bff152
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-05-01 01:19:01 -0400

    commit

---
+++ 04-operators.js
@@ -0,0 +1,33 @@
+//operadores Aritmeticos
+let a = 4
+let b = 3
+
+console.log(a + b)
+console.log(b - a)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)
+
+a++
+console.log(a)
+b--
+console.log(b)
+
+//operadores de asignacion
+let variable = 4
+variable += 7
+console.log(variable)
+
+// Operadores de comparacion
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a === a)
+console.log(a != b)
+console.log(a != 6)
+console.log(a !== "5")
+
+// Truthy values 
